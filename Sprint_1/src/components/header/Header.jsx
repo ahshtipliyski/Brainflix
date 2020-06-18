63acdde1c110dceb3de491faafd70b2fadcc679d
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-06-18 00:27:03 -0300

    Created components. Set up Mobile version.

---
+++ Sprint_1/src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../../App.scss'
+import '../header/Header.scss';
+import logo from '../../assets/Logo/Logo-brainflix.svg';
+import userIcon from '../../assets/Images/Mohan-muruge.jpg';
+import search from '../../assets/Icons/SVG/Icon-search.svg'
+
+
+function Header() {
+  return (
+    <nav className="nav-bar">
+      <div className="nav-bar__logo-container"><a href="#"><img src={logo} alt="Brainflix logo" className="nav-bar__logo"/></a></div>
+      <div className="nav-bar__input-container">
+        <input type="text" className="nav-bar__input" placeholder= "      Search" />
+      </div>
+      <div className="nav-bar__account">
+        <button className="nav-bar__upload-button"><span className="nav-bar__plus">+</span> UPLOAD</button>
+        <img src={userIcon} alt="User Icon" className="nav-bar__icon"/>
+      </div>
+    </nav>
+  );
+}
+
+
+export default Header;
---
+++ Sprint_1/src/components/hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import '../hero/Hero.scss';
+import mainVideo from '../../assets/Video/BrainStation-Sample-Video.mp4';
+import posterVideo from '../../assets/Images/video-list-0.jpg';
+// import videoPlay from '../../assets/Icons/SVG/Icon-play.svg';
+// import videoFull from '../../assets/Icons/SVG/Icon-fullscreen.svg';
+// import videoSound from '../../assets/Icons/SVG/Icon-volume.svg';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero__background"></div>
+      <video controls
+        src={mainVideo} 
+        type="video/mp4" 
+        poster={posterVideo}
+        className="hero__video">
+        Sorry, your browser doesn't support embedded videos.
+      </video>
+      {/* <div className='hero__controls'>
+        <img src={videoPlay} id='play' alt="videoPlay"/>
+        <div className='videoProcess'>
+           <div id='progressbar'></div>
+           <span>0:00 / 0:42</span>
+        </div>
+        <div id='side'><img src={videoFull} alt="videoFull"/><img src={videoSound} alt="videoSound"/></div>
+      </div> */}
+    </section>
+  );
+}
+export default Hero;
