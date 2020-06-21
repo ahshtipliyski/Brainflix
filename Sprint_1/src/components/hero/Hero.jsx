92e00e96cc6a6fef4c3599c48ad397911351703f
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-06-21 01:29:14 -0300

    Creating video player buttons. Tablet and Desktop breakpoints are not done yet.

--- Sprint_1/src/components/hero/Hero.jsx
+++ Sprint_1/src/components/hero/Hero.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import '../Hero/Hero.scss';
 import axios from 'axios';
+import play from '../../assets/Icons/SVG/Icon-play.svg';
+import fullscreen from '../../assets/Icons/SVG/Icon-fullscreen.svg';
+import volume from '../../assets/Icons/SVG/Icon-volume.svg';
 
 class Hero extends React.Component {
   state = {
@@ -16,13 +19,21 @@ class Hero extends React.Component {
     return (
       <section className="hero">
         <div className="hero__background">
-        <video // controls
-          src={this.state.input.video} 
-          type="video/mp4" 
-          poster={this.state.input.image}
-          className="hero__video">
-          Sorry, your browser doesn't support embedded videos.
-        </video>
+          <video // controls
+            src={this.state.input.video} 
+            type="video/mp4" 
+            poster={this.state.input.image}
+            className="hero__video">
+            Sorry, your browser doesn't support embedded videos.
+          </video>
+          <div className="hero__controls">
+            <img src={play} className="hero__play" alt="Play Button"/>
+            <div className="hero__video-process">
+              <div className="hero__progress-bar"></div>
+            <span className="hero__progress-time">0:00 / 0:42</span>
+            </div>
+            <div className="hero__settings"><img src={fullscreen} alt="Video fullscreen button"/><img src={volume} alt="Volume control button"/></div>
+          </div>
         </div>
       </section>
     );
